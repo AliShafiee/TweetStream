055faca2269766253bd70032faee49044fa70f4e
Ali Shafiee <alishafiee@Alis-MacBook-Pro.local>
2022-05-20 16:03:33 +0430

    setup project

---
+++ TweetStream/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TweetStream
+//
+//  Created by Ali Shafiee on 2/30/1401 AP.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
